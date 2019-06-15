9715aedb14f83b50c7a825eb648d75a1acb6066a
ema2159 <ema2159@gmail.com>
2019-06-14 20:46:43 -0600

    Added emmet-mode

--- core/4_emmacs_modes.el
+++ core/4_emmacs_modes.el
@@ -5,27 +5,21 @@
 ;; - Anaconda Mode
 ;; - CSV Mode
 ;; - Elpy
+;; - Emmet Mode
 ;; - Irony
-;; - Js2 mode
-;; - Specman mode
-;; - Tern mode
+;; - Js2 Mode
+;; - Specman Mode
+;; - Tern Mode
 
 ;;; Code:
-;; Specman mode
+;; Specman Mode
 (add-to-list 'load-path "~/.emacs.d/specman")
-(load "specman-mode")
 (put 'specman-mode 'derived-mode-parent 'prog-mode)
+(load "specman-mode")
 (add-to-list 'auto-mode-alist '("\\.e\\'" . specman-mode))
 (add-to-list 'auto-mode-alist '("\\.ecom\\'" . specman-mode))
 (add-hook 'specman-mode-hook (lambda () (use-local-map nil)))
 (add-hook 'specman-mode-hook 'yas-minor-mode)
-
-;; Elpy
-(use-package elpy
-  :ensure t
-  :defer t
-  :init
-  (advice-add 'python-mode :before 'elpy-enable))
 
 ;; Irony
 (use-package irony
@@ -35,18 +29,18 @@
    (c++-mode . irony-mode)
    (irony-mode . irony-cdb-autosetup-compile-options)))
 
-;; Csv mode
+;; Csv Mode
 (use-package csv-mode
   :ensure t
   :mode
   ("\\.[Cc][Ss][Vv]\\'" . csv-mode))
 
-;; Tern mode
+;; Tern Mode
 (use-package tern
   :ensure t
   :after js-mode)
 
-;; Js2 mode
+;; Js2 Mode
 (use-package js2-mode
   :ensure t
   :mode
@@ -57,13 +51,20 @@
                            (tern-mode)
                            (company-mode))))
 
-;; Anaconda mode
+;; Anaconda Mode
 (use-package anaconda-mode
   :ensure t
   :config
   (pythonic-activate "/home/emmanuel/anaconda3/envs/ml")
   :hook
   (python-mode . anaconda-mode))
+
+;; Emmet Mode
+(use-package emmet-mode
+  :ensure t
+  :hook
+  (sgml-mode . emmet-mode)
+  (css-mode . emmet-mode))
 
 ;; ESS
 ;; (use-package ess
