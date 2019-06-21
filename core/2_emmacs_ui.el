903da69cc58985e6ae32f78857f77c353abbb9b3
ema2159 <ema2159@gmail.com>
2019-06-21 10:46:10 -0600

    Enabled all-the-icons-dired

--- core/2_emmacs_ui.el
+++ core/2_emmacs_ui.el
@@ -75,7 +75,7 @@
       ;; if graphic
       (use-package all-the-icons)
       (use-package all-the-icons-dired
-	:hook (dired-mode . treemacs-icons-dired-mode))))
+	:hook (dired-mode . all-the-icons-dired-mode))))
 
 ;; Dired Hacks
 (use-package dired-hacks-utils
