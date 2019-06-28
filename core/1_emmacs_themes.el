42d2ff04ca0bd25c9b221b5051223ab9b25d192e
ema2159 <ema2159@gmail.com>
2019-06-28 12:05:41 -0600

    Added Kaolin themes

--- core/1_emmacs_themes.el
+++ core/1_emmacs_themes.el
@@ -9,14 +9,17 @@
 
 ;;; Code:
 ;; Doom themes
-;; (use-package doom-themes
+(use-package doom-themes
+  :ensure t
+  :defer t
+  :config
+  (doom-themes-org-config))
+
+;; (use-package kaolin-themes
 ;;   :ensure t
 ;;   :defer t
 ;;   :config
-;;   (doom-themes-org-config)
-;;   :custom-face
-;;   (tabbar-unselected          ((t (:foreground "#A0B3C5" :background "#181E24" :slant italic))))
-;;   (tabbar-unselected-modified ((t (:background "#181E24" :foreground "#d02b61" :slant italic)))))
+;;   (kaolin-treemacs-theme))
 
 ;; Solarized theme
 ;; (use-package solarized-theme
@@ -28,22 +31,28 @@
   ;; :ensure t
   ;; :defer t)
 
-(require 'spacemacs-dark-theme)
+;; (require 'spacemacs-dark-theme)
+;; (setq spacemacs-theme-comment-bg nil)
 ;; (require 'atom-one-dark-theme)
 ;; (require 'solarized-dark-theme)
-(setq spacemacs-theme-comment-bg nil)
+(require 'doom-themes)
+;; (require 'kaolin-themes)
 
 ;; Theme
 (if (display-graphic-p)
     (progn
-    ;; if graphic
-      (load-theme 'spacemacs-dark t))
+      ;; if graphic
+      ;; (load-theme 'spacemacs-dark t))
       ;; (load-theme 'solarized-dark t))
       ;; (load-theme 'doom-one t))
       ;; (load-theme 'doom-city-lights t))
-      ;; (load-theme 'doom-dracula t))
-      ;; (load-theme 'atom-one-dark t))
-    ;; else (optional)
+      ;; (load-theme 'doom-molokai t))
+      ;; (load-theme 'kaolin-eclipse t))
+      (load-theme 'doom-dracula t))
+  ;; (load-theme 'doom-Iosvkem t))
+  ;; (load-theme 'doom-solarized-dark t))
+  ;; (load-theme 'atom-one-dark t))
+  ;; else (optional)
   (load-theme 'atom-one-dark t))
 
 (provide '1_emmacs_themes)
