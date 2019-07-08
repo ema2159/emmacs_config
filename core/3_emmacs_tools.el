ceb89e10cd31575c650cced17ef944154908b42c
ema2159 <ema2159@gmail.com>
2019-07-07 22:24:54 -0600

    Add: Smartparens hydra

--- core/3_emmacs_tools.el
+++ core/3_emmacs_tools.el
@@ -181,6 +181,61 @@
 ;; Smart parens
 (use-package smartparens
   :ensure t
+  :config
+  (defhydra hydra-smartparens (:hint nil :color pink)
+    "
+Moving^^^^                       Slurp & Barf^^    Wrapping^^                 Sexp juggling^^^^                Destructive^^^^        Exit
+――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+ [_a_] beginning  [_n_] down    │ [_h_] bw slurp │ [_R_]   rewrap          │ [_S_] split   [_t_] transpose  │ [_c_] change inner  │ [_w_] copy
+ [_e_] end        [_N_] bw down │ [_H_] bw barf  │ [_u_]   unwrap          │ [_s_] splice  [_A_] absorb     │ [_C_] change outer  │ [_g_] quit
+ [_f_] forward    [_p_] up      │ [_l_] slurp    │ [_U_]   bw unwrap       │ [_r_] raise   [_E_] emit       │ [_k_] kill          │ [_q_] quit
+ [_b_] backward   [_P_] bw up   │ [_L_] barf     │ [_(__{__[_] wrap (){}[]     │ [_j_] join    [_o_] convolute  │ [_K_] bw kill"
+    ;; Moving
+    ("a" sp-beginning-of-sexp)
+    ("e" sp-end-of-sexp)
+    ("f" sp-forward-sexp)
+    ("b" sp-backward-sexp)
+    ("n" sp-down-sexp)
+    ("N" sp-backward-down-sexp)
+    ("p" sp-up-sexp)
+    ("P" sp-backward-up-sexp)
+    
+    ;; Slurping & barfing
+    ("h" sp-backward-slurp-sexp)
+    ("H" sp-backward-barf-sexp)
+    ("l" sp-forward-slurp-sexp)
+    ("L" sp-forward-barf-sexp)
+    
+    ;; Wrapping
+    ("R" sp-rewrap-sexp)
+    ("u" sp-unwrap-sexp)
+    ("U" sp-backward-unwrap-sexp)
+    ("(" sp-wrap-round)
+    ("{" sp-wrap-curly)
+    ("[" sp-wrap-square)
+    
+    ;; Sexp juggling
+    ("S" sp-split-sexp)
+    ("s" sp-splice-sexp)
+    ("r" sp-raise-sexp)
+    ("j" sp-join-sexp)
+    ("t" sp-transpose-sexp)
+    ("A" sp-absorb-sexp)
+    ("E" sp-emit-sexp)
+    ("o" sp-convolute-sexp)
+    
+    ;; Destructive editing
+    ("c" sp-change-inner :exit t)
+    ("C" sp-change-enclosing :exit t)
+    ("k" sp-kill-sexp)
+    ("K" sp-backward-kill-sexp)
+    ("w" sp-copy-sexp)
+
+    ;; Quitting
+    ("<escape>" nil)
+    ("C-x k" kill-buffer-and-window :exit t)
+    ("q" nil)
+    ("g" nil))
   :hook
   (python-mode . smartparens-mode)
   (emacs-lisp-mode . smartparens-mode)
@@ -188,7 +243,9 @@
   (c++-mode . smartparens-mode)
   (specman-mode . smartparens-mode)
   (js2-mode . smartparens-mode)
-  (html-mode . smartparens-mode))
+  (html-mode . smartparens-mode)
+  :bind ((:map prog-mode-map
+	       ("C-(" . hydra-smartparens/body))))
 
 ;; Highlight indent guides
 (use-package highlight-indent-guides
