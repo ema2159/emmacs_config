f7eef9827f2277eb8d2d4ba44363a024566cc0c8
ema2159 <ema2159@gmail.com>
2020-01-21 15:38:18 -0600

    Add: LSP support pakages

--- core/3_emmacs_tools.el
+++ core/3_emmacs_tools.el
@@ -323,13 +323,30 @@ Moving^^^^                       Slurp & Barf^^    Wrapping^^                 Se
   (magit-mode . turn-off-evil-snipe-override-mode))
 
 (when emmacs-activate-lsp
+  ;; LSP Mode
   (use-package lsp-mode
     :commands lsp
     :custom
     (lsp-auto-guess-root nil)
     (lsp-prefer-flymake nil) ; Use flycheck instead of flymake
-    :bind (:map lsp-mode-map ("C-c C-f" . lsp-format-buffer))
-    :hook ((python-mode) . lsp)))
+    :hook
+    ((python-mode) . lsp)
+    ((js-mode) . lsp))
+
+  ;; LSP UI
+  (use-package lsp-ui
+    :commands lsp-ui-mode
+    :config
+    (lsp-ui-sideline))
+
+  ;; Company LSP
+  (use-package company-lsp :commands company-lsp)
+
+  ;; LSP Treemacs
+  (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
+
+  ;; LSP Ivy
+  (use-package lsp-ivy))
 
 (unless emmacs-activate-lsp
   ;; Company Anaconda
