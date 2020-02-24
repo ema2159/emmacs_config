8f76e9f7d049575a6e90684105d9c33066b02674
ema2159 <ema2159@gmail.com>
2020-02-23 23:28:44 -0600

    Mod: Use fuzzy search for ivy completion (except for swiper).

--- core/2_emmacs_ui.el
+++ core/2_emmacs_ui.el
@@ -297,78 +297,81 @@ _v_iew             │ ^ ^              │                │                 �
   (after-init . doom-modeline-mode))
 
 ;; Centaur Tabs
-(use-package centaur-tabs
-  :config
-  (setq centaur-tabs-style "bar"
-	centaur-tabs-height 32
-	centaur-tabs-set-icons t
-	centaur-tabs-set-modified-marker t
-	centaur-tabs-show-navigation-buttons t
-	centaur-tabs-set-bar 'under
-	x-underline-at-descent-line t
-	centaur-tabs-left-edge-margin nil)
-  (centaur-tabs-headline-match)
-  ;; (setq centaur-tabs-gray-out-icons 'buffer)
-  ;; (centaur-tabs-enable-buffer-reordering)
-  ;; (setq centaur-tabs-adjust-buffer-order t)
-  (centaur-tabs-mode t)
-  (setq uniquify-separator "/")
-  (setq uniquify-buffer-name-style 'forward)
-  (defun centaur-tabs-buffer-groups ()
-    "`centaur-tabs-buffer-groups' control buffers' group rules.
+(use-package powerline)
+(when (display-graphic-p)
+  (use-package centaur-tabs
+    :config
+    (setq centaur-tabs-style "bar"
+	  centaur-tabs-height 32
+	  centaur-tabs-set-icons t
+	  centaur-tabs-set-modified-marker t
+	  centaur-tabs-show-navigation-buttons t
+	  centaur-tabs-set-bar 'under
+	  x-underline-at-descent-line t
+	  centaur-tabs-left-edge-margin nil)
+    (centaur-tabs-headline-match)
+    ;; (setq centaur-tabs-gray-out-icons 'buffer)
+    ;; (centaur-tabs-enable-buffer-reordering)
+    ;; (setq centaur-tabs-adjust-buffer-order t)
+    (centaur-tabs-mode t)
+    (setq uniquify-separator "/")
+    (setq uniquify-buffer-name-style 'forward)
+    (defun centaur-tabs-buffer-groups ()
+      "`centaur-tabs-buffer-groups' control buffers' group rules.
 
 Group centaur-tabs with mode if buffer is derived from `eshell-mode' `emacs-lisp-mode' `dired-mode' `org-mode' `magit-mode'.
 All buffer name start with * will group to \"Emacs\".
 Other buffer group by `centaur-tabs-get-group-name' with project name."
-    (list
-     (cond
-      ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
-      ;; "Remote")
-      ((or (string-equal "*" (substring (buffer-name) 0 1))
-	   (memq major-mode '(magit-process-mode
-			      magit-status-mode
-			      magit-diff-mode
-			      magit-log-mode
-			      magit-file-mode
-			      magit-blob-mode
-			      magit-blame-mode
-			      )))
-       "Emacs")
-      ((derived-mode-p 'prog-mode)
-       "Editing")
-      ((derived-mode-p 'dired-mode)
-       "Dired")
-      ((memq major-mode '(helpful-mode
-			  help-mode))
-       "Help")
-      ((memq major-mode '(org-mode
-			  org-agenda-clockreport-mode
-			  org-src-mode
-			  org-agenda-mode
-			  org-beamer-mode
-			  org-indent-mode
-			  org-bullets-mode
-			  org-cdlatex-mode
-			  org-agenda-log-mode
-			  diary-mode))
-       "OrgMode")
-      (t
-       (centaur-tabs-get-group-name (current-buffer))))))
-  :hook
-  (dashboard-mode . centaur-tabs-local-mode)
-  (term-mode . centaur-tabs-local-mode)
-  (calendar-mode . centaur-tabs-local-mode)
-  (org-agenda-mode . centaur-tabs-local-mode)
-  (helpful-mode . centaur-tabs-local-mode)
-  :bind
-  ("C-<prior>" . centaur-tabs-backward)
-  ("C-<next>" . centaur-tabs-forward)
-  ("C-c t s" . centaur-tabs-counsel-switch-group)
-  ("C-c t p" . centaur-tabs-group-by-projectile-project)
-  ("C-c t g" . centaur-tabs-group-buffer-groups)
-  (:map evil-normal-state-map
-	("g t" . centaur-tabs-forward)
-	("g T" . centaur-tabs-backward)))
+      (list
+       (cond
+	;; ((not (eq (file-remote-p (buffer-file-name)) nil))
+	;; "Remote")
+	((or (string-equal "*" (substring (buffer-name) 0 1))
+	     (memq major-mode '(magit-process-mode
+				magit-status-mode
+				magit-diff-mode
+				magit-log-mode
+				magit-file-mode
+				magit-blob-mode
+				magit-blame-mode
+				)))
+	 "Emacs")
+	((derived-mode-p 'prog-mode)
+	 "Editing")
+	((derived-mode-p 'dired-mode)
+	 "Dired")
+	((memq major-mode '(helpful-mode
+			    help-mode))
+	 "Help")
+	((memq major-mode '(org-mode
+			    org-agenda-clockreport-mode
+			    org-src-mode
+			    org-agenda-mode
+			    org-beamer-mode
+			    org-indent-mode
+			    org-bullets-mode
+			    org-cdlatex-mode
+			    org-agenda-log-mode
+			    diary-mode))
+	 "OrgMode")
+	(t
+	 (centaur-tabs-get-group-name (current-buffer))))))
+    :hook
+    (dashboard-mode . centaur-tabs-local-mode)
+    (term-mode . centaur-tabs-local-mode)
+    (calendar-mode . centaur-tabs-local-mode)
+    (org-agenda-mode . centaur-tabs-local-mode)
+    (helpful-mode . centaur-tabs-local-mode)
+    :bind
+    ("C-<prior>" . centaur-tabs-backward)
+    ("C-<next>" . centaur-tabs-forward)
+    ("C-c t s" . centaur-tabs-counsel-switch-group)
+    ("C-c t p" . centaur-tabs-group-by-projectile-project)
+    ("C-c t g" . centaur-tabs-group-buffer-groups)
+    (:map evil-normal-state-map
+	  ("g t" . centaur-tabs-forward)
+	  ("g T" . centaur-tabs-backward)))
+  )
 
 ;; Which key
 (use-package which-key
@@ -420,8 +423,7 @@ Other buffer group by `centaur-tabs-get-group-name' with project name."
 					2
 					centaur-tabs-bar-height))
 	  (centaur-tabs-init-tabsets-store)
-	  ;; (centaur-tabs-display-update)
-	  (centaur-tabs-force-update)
+	  (centaur-tabs-display-update)
 	  (centaur-tabs-headline-match))
       (error "Problem loading theme %s" x)))
   (defun counsel-load-theme ()
@@ -434,7 +436,6 @@ Usable with `ivy-resume', `ivy-next-line-and-call' and
 		      (custom-available-themes))
 	      :action #'counsel-load-theme-action
 	      :caller 'counsel-load-theme
-	      ;; :update-fn (lambda() (counsel-load-theme-action (ivy-state-current ivy-last)))
 	      ))
   :bind
   (("\C-x\ \C-r" . counsel-recentf)
@@ -468,7 +469,10 @@ Usable with `ivy-resume', `ivy-next-line-and-call' and
   (("\C-s" . swiper)
    ("C-c C-r" . ivy-resume)
    ("C-x b" . ivy-switch-buffer))
-  :config)
+  :config
+  (setq ivy-re-builders-alist
+	'((swiper . ivy--regex-plus)
+	  (t      . ivy--regex-fuzzy))))
 
 ;; Ivy Posframe
 (use-package ivy-posframe
@@ -547,22 +551,22 @@ Usable with `ivy-resume', `ivy-next-line-and-call' and
 
 ;; Improve python-mode syntax highlighting
 (font-lock-add-keywords 'python-mode
-  '(("-?\\b[0-9]+\\.?" . highlight-numbers-number)
-    ("[\.\,\;\:\*\|\&\!\(\)\{\}\=\$\<\>\'\#\%\-\+]\\|\\]\\|\\[" . font-lock-punctuation-face)
-    ("\\([A-Za-z][A-Za-z0-9_]*\\)[ \t\n]*\\((.*)\\)"
-     (1 font-lock-function-name-face))))
+			'(("-?\\b[0-9]+\\.?" . highlight-numbers-number)
+			  ("[\.\,\;\:\*\|\&\!\(\)\{\}\=\$\<\>\'\#\%\-\+]\\|\\]\\|\\[" . font-lock-punctuation-face)
+			  ("\\([A-Za-z][A-Za-z0-9_]*\\)[ \t\n]*\\((.*)\\)"
+			   (1 font-lock-function-name-face))))
 ;; Improve js-mode syntax highlighting
 (font-lock-add-keywords 'rjsx-mode
-  '(("[\.\,\;\:\*\|\&\!\(\)\{\}\=\$\<\>\'\#\%\-\+]\\|\\]\\|\\[" . font-lock-punctuation-face)))
+			'(("[\.\,\;\:\*\|\&\!\(\)\{\}\=\$\<\>\'\#\%\-\+]\\|\\]\\|\\[" . font-lock-punctuation-face)))
 ;; Improve c-mode syntax highlighting
 (font-lock-add-keywords 'c-mode
-  '(("-?\\b[0-9]+\\.?" . highlight-numbers-number)
-    ("[\.\,\;\:\*\|\&\!\(\)\{\}\=\$\<\>\'\#\%\-\+\@]\\|\\]\\|\\[" . font-lock-punctuation-face)))
+			'(("-?\\b[0-9]+\\.?" . highlight-numbers-number)
+			  ("[\.\,\;\:\*\|\&\!\(\)\{\}\=\$\<\>\'\#\%\-\+\@]\\|\\]\\|\\[" . font-lock-punctuation-face)))
 ;; Improve verilog-mode syntax highlighting
 (font-lock-add-keywords 'verilog-mode
-  '(("-?\\b[0-9]+\\.?" . highlight-numbers-number)
-    ("[\.\,\;\:\*\|\&\!\(\)\{\}\=\$\<\>\'\#\%\-\+\@]\\|\\]\\|\\[" . font-lock-punctuation-face)))
+			'(("-?\\b[0-9]+\\.?" . highlight-numbers-number)
+			  ("[\.\,\;\:\*\|\&\!\(\)\{\}\=\$\<\>\'\#\%\-\+\@]\\|\\]\\|\\[" . font-lock-punctuation-face)))
 
- 
+
 (provide '2_emmacs_ui)
 ;;; 2_emmacs-ui.el ends here
