85b0c3b1964dc26cfb68f589c13b1ddff4d069d1
Emmanuel Bustos <ema2159@gmail.com>
2021-01-15 16:03:16 +0200

    Add: Activate web-mode in ejs mode

--- core/4_emmacs_modes.el
+++ core/4_emmacs_modes.el
@@ -100,7 +100,9 @@
 
 ;; Web Mode
 (use-package web-mode
-  :mode ("\\.html$" . web-mode)
+  :mode
+  ("\\.html$" . web-mode)
+  ("\\.ejs$" . web-mode)
   :init
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
